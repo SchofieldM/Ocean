5c9b45408deb63e76a4921a6eb93cff4c0e3755e
SchofieldM <36042254+SchofieldM@users.noreply.github.com>
2018-02-14 16:16:55 -0500

    Add files via upload

--- Ocean/src/main/com/utilities/RNG.java
+++ Ocean/src/main/com/utilities/RNG.java
@@ -6,10 +6,18 @@ import java.util.Random;
  * For random number generation
  * 
  * @author Matthew Schofield
- * @version 2.7.18
+ * @version 2.14.18
 */
 public class RNG {
 
+// int
+	/**
+	 * Generates a random int
+	 * 
+	 * @param min, minimum int inclusive
+	 * @param max, maximum int inclusive
+	 * @return returns an int inclusively between min and max
+	 */
 	public static int randomInt(int min, int max)
 	{
 		return (new Random()).nextInt(max) + min;
