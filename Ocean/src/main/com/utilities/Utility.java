5228cc0a0f1e95bda39457f4b81e6d3e89920743
SchofieldM <36042254+SchofieldM@users.noreply.github.com>
2018-02-20 22:09:18 -0500

    Add files via upload

---
+++ Ocean/src/main/com/utilities/Utility.java
@@ -0,0 +1,26 @@
+package main.com.utilities;
+
+import java.util.HashMap;
+
+public class Utility {
+	
+	public Object[] orderHashMapAsArray(HashMap<Object, Integer> map)
+	{
+		Object[] array = new Object[map.size()];
+		
+		for(Object key : map.keySet()) {
+			if(array[0] == null) {
+				array[0]
+			}
+		}
+		
+		return array;
+	}
+	
+	public Object[] sort(Object[] toSort)
+	{
+		Object[] sorted = new ObtoSort.length];
+		
+	}
+
+}
